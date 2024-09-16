c49242e00230f6c6b500c9613a5bb8e71d22a7d0
marllonvinicius <viniciussousa.mvc@gmail.com>
2024-09-16 00:06:55 -0300

    Add image and text on "Yes!" click and hide elements on page load, works in mobiles devices

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-let card = document.getElementById("card");
-
-// Exibir a mensagem caso seja selecionado o 'sim'
-document.querySelector("#yes").addEventListener("click", function () {
+document.addEventListener("DOMContentLoaded", function () {
+  let card = document.getElementById("card");
   let question = document.getElementById("question");
   let message = document.getElementById("answer");
+  let loveImage = document.getElementById("love-image");
 
-  card.style.backgroundImage =
-    "url('https://img1.picmix.com/output/stamp/normal/5/1/0/1/1171015_f458c.gif')";
-  question.style.display = "none";
-  message.style.display = "flex";
-});
+  // Inicialmente, a imagem e o texto de resposta estão ocultos
+  message.style.display = "none";
+  loveImage.style.display = "none";
 
-// Movimento aleatório do botão 'não'
-document.querySelector("#no").addEventListener("mouseover", function () {
-  let width = window.innerWidth - 50;
-  let height = window.innerHeight - 50;
+  // Exibir a mensagem e a imagem caso seja selecionado o 'sim'
+  document.querySelector("#yes").addEventListener("click", function () {
+    card.style.backgroundImage = "url('https://img1.picmix.com/output/stamp/normal/5/1/0/1/1171015_f458c.gif')";
+    question.style.display = "none";
+    message.style.display = "flex";
+    loveImage.style.display = "block"; // Exibe a imagem
+  });
 
-  this.style.position = "absolute";
-  this.style.top = Math.random() * width + "px";
-  this.style.left = Math.random() * height + "px";
+  // Movimento aleatório do botão 'não'
+  document.querySelector("#no").addEventListener("mouseover", function () {
+    let width = window.innerWidth - 50;  // Largura da janela
+    let height = window.innerHeight - 50; // Altura da janela
+
+    this.style.position = "absolute";
+    this.style.top = Math.random() * height + "px";  // Usar height para 'top'
+    this.style.left = Math.random() * width + "px";  // Usar width para 'left'
+  });
 });
